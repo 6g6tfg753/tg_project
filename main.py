cf129f6ee7e4623520c8bd3e260208f27d3b17e6
Дмитрий <dmitreevmaxsim199@gmail.com>
2025-04-21 16:58:14 +0300

    lists

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from telegram.ext import Application, ConversationHandler, filters, CommandHandl
 from telegram import ReplyKeyboardMarkup
 import logging
 import sqlite3
+import requests
+import aiohttp
 
 
 class TG_BOT():
@@ -11,69 +13,175 @@ class TG_BOT():
             format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
         )
 
-        logger = logging.getLogger(__name__)
+        self.logger = logging.getLogger(__name__)
         self.con = sqlite3.connect('film_db.sqlite')
         self.cur = self.con.cursor()
         self.user_response = []
 
     async def start(self, update, context):
         self.user_name = [update.message][0]['chat']['first_name']
-        reply_keyboard = [['/make_list', '/button2']]
+        reply_keyboard = [['/get_list_name', '/view_lists', '/geocoder']]
         markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
         await update.message.reply_text(
             "здесь будет описание и список команд",
             reply_markup=markup)
 
+    async def get_list_name(self, update, context):
+        self.user_name = [update.message][0]['chat']['first_name']
+        new_item = """SELECT list_name FROM films WHERE tg_name = ?"""
+        lists = set(self.cur.execute(new_item, (self.user_name,)).fetchall())
+        reply_keyboard = []
+        for i in lists:
+            reply_keyboard.append([f"{str(i)[2:-3]}"])
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+        await update.message.reply_text(
+            'Выберете список. Чтобы создать новый список введите его название',
+            reply_markup=markup)
+        return 1
+
     async def make_list(self, update, context):
+        self.list_name = update.message.text
         reply_keyboard = [['/stop']]
         markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
         await update.message.reply_text(
             'Введите название фильма',
             reply_markup=markup)
-        return 1
+        return 2
 
     async def get_data(self, update, context):
+        self.user_response = []
         self.user_response.append(update.message.text)
         reply_keyboard = [['Комедия', 'Детектив', 'Другое']]
         markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
         await update.message.reply_text(
-            'Выберете жанр',
+            'Выберете жанр фильма',
             reply_markup=markup)
-        return 2
+        return 3
 
     async def add_to_bd(self, update, context):
         self.user_name = [update.message][0]['chat']['first_name']
         self.user_response.append(update.message.text)
         await update.message.reply_text(
             f"Успех")
-        new_item = """INSERT INTO films(tg_name, film_name, genre) VALUES (?, ?, ?)"""
-        self.cur.execute(new_item, (self.user_name, self.user_response[0], self.user_response[1]))
+        new_item = """INSERT INTO films(tg_name, film_name, genre, list_name) VALUES (?, ?, ?, ?)"""
+        self.cur.execute(new_item, (self.user_name, self.user_response[0], self.user_response[1], self.list_name))
         self.con.commit()
-        return ConversationHandler.END
-
-    async def stop(self, update, context):
-        reply_keyboard = [['/make_list', '/button2']]
+        reply_keyboard = [['/get_list_name', '/view_lists']]
         markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
         await update.message.reply_text(
             'Выберете, что хотите сделать',
             reply_markup=markup)
         return ConversationHandler.END
 
+    async def stop(self, update, context):
+        reply_keyboard = [['/make_list', '/view_lists', "/geocoder"]]
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+        await update.message.reply_text(
+            'Выберете, что хотите сделать',
+            reply_markup=markup)
+        return ConversationHandler.END
 
+    async def help(self, update, context):
+        await update.message.reply_text(
+            f"Я бот.\n"
+            f"Что умеет этот бот:\n"
+            f"/make_list -- добавить в список фильм\n"
+            f"/geocoder (Город) -- найти картинку города\n"
+            f"/help -- помощь с ботом\n")
+
+    def get_ll_spn(self, toponym):
+        ll = list(map(lambda x: str(x), toponym["Point"]["pos"].split()))
+        lowercorner = list(map(lambda x: float(x), toponym["boundedBy"]["Envelope"]["lowerCorner"].split()))
+        uppercorner = list(map(lambda x: float(x), toponym["boundedBy"]["Envelope"]["upperCorner"].split()))
+        spn = list(map(lambda x: str(x), [uppercorner[0] - lowercorner[0], uppercorner[1] - lowercorner[1]]))
+        return ll, spn
+
+    async def geocoder(self, update, context):
+        geocoder_uri = "http://geocode-maps.yandex.ru/1.x/"
+        response = await self.get_response(geocoder_uri, params={
+            "apikey": "8013b162-6b42-4997-9691-77b7074026e0",
+            "format": "json",
+            "geocode": update.message.text
+        })
+        if str(update.message.text)[10:] == "":
+            print("error! no geo-object!")
+            await context.bot.sendMessage(
+                update.message.chat_id,  f"Чтобы создать запрос вы должны ввести название объекта,"
+                                         f" картинку которого вы хотите вывести, например:\n\n"
+                                         f"\geocoder Якутск\n\n"
+                                         f"Попробуйте ещё раз)")
+            return
+        elif (response['response']['GeoObjectCollection']["featureMember"] == []):
+            print("error! geo-object not found!")
+            await context.bot.sendMessage(
+                update.message.chat_id,  f"Чтобы создать запрос вы должны ввести название объекта,"
+                                         f" картинку которого вы хотите вывести, например:\n"
+                                         f"\geocoder Якутск\n\n"
+                                         f"Попробуйте ещё раз)")
+
+            return
+        toponym = response['response']['GeoObjectCollection']["featureMember"][0]["GeoObject"]
+        ll, spn = self.get_ll_spn(toponym)
+        static_api_request = (f"http://static-maps.yandex.ru/1.x/?ll={str(ll[0]) + ',' + str(ll[1])}"
+                              f"&spn={str(spn[0]) + ',' + str(spn[1])}&l=map")
+        await context.bot.send_photo(
+            update.message.chat_id, static_api_request,
+            caption=f"{(response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty']['GeocoderMetaData']['text'])}"
+        )
+
+    async def get_response(self, url, params):
+        self.logger.info(f"getting {url}")
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params) as resp:
+                return await resp.json()
+
+    async def view_lists(self, update, context):
+        self.user_name = [update.message][0]['chat']['first_name']
+        new_item = """SELECT list_name FROM films WHERE tg_name = ?"""
+        lists = set(self.cur.execute(new_item, (self.user_name,)).fetchall())
+        reply_keyboard = []
+        for i in lists:
+            reply_keyboard.append([f"{str(i)[2:-3]}"])
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+        await update.message.reply_text(
+            'Выберете список',
+            reply_markup=markup)
+        return 11
+
+    async def return_list(self, update, context):
+        list_name = update.message.text
+        new_item = """SELECT film_name, genre FROM films WHERE tg_name = ? AND list_name = ?"""
+        result = (self.cur.execute(new_item, (self.user_name, list_name,)).fetchall())
+        list_content = []
+        for i in result:
+            list_content.append(f"название: {i[0]}, жанр: {i[1]}")
+        await update.message.reply_text(f"{list_content}")
+        reply_keyboard = [['/get_list_name', '/view_lists']]
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+        await update.message.reply_text(
+            'Выберете, что хотите сделать',
+            reply_markup=markup)
+        return ConversationHandler.END
 
     def main(self):
         conv_handler = ConversationHandler(
-            entry_points=[CommandHandler('make_list', self.make_list)],
+            entry_points=[CommandHandler('get_list_name', self.get_list_name), CommandHandler('view_lists', self.view_lists)],
             states={
-                1: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.get_data)],
-                2: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.add_to_bd)]
+                1: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.make_list)],
+                2: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.get_data)],
+                3: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.add_to_bd)],
+                11: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.return_list)]
 
             },
             fallbacks=[CommandHandler('stop', self.stop)]
         )
         application = Application.builder().token(TOKEN).build()
         application.add_handler(conv_handler)
+
         application.add_handler(CommandHandler("start", self.start))
+        application.add_handler(CommandHandler("help", self.help))
+        application.add_handler(CommandHandler("geocoder", self.geocoder))
+
         application.run_polling()
 
 
